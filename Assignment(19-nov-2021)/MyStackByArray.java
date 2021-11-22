b0a97fe6b50304f20b5b20f234195f1c984d0429
Amit kumar <amit68568@gmail.com>
2021-11-22 12:00:36 +0530

    Assignment

--- Assignment(19-nov-2021)/MyStackByArray.java
+++ Assignment(19-nov-2021)/MyStackByArray.java
@@ -45,6 +45,31 @@ public class MyStackByArray {
            return false;
         }
     }
+
+    int getAvg(){
+        int sum=0;
+        for (int i = 0; i <= top; i++) {
+            sum=sum+a[i];
+        }
+        int avg=sum/(top+1);
+       
+        
+        return avg;
+
+    }
+    int getMax(){
+      
+        int max=a[1];
+        int i=0;
+        while (i<=top) {
+            if(max<a[i]){
+                max=a[i];
+               
+        }
+        i++;
+    }
+        return max;
+    }
     public static void main(String[] args) {
         MyStackByArray s1=new MyStackByArray(10);
         s1.push(10);
@@ -52,6 +77,8 @@ public class MyStackByArray {
         s1.push(12);
         s1.push(13);
         s1.push(14);
+      System.out.println(s1.getAvg());  
+      System.out.println(s1.getMax());  
       System.out.println(s1.peek());  
       System.out.println(s1.pop());  
       System.out.println(s1.peek());  
